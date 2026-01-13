9c7adcd390372d7842e51f1a0f2975f350afd31f
hudea <hudealex1@gmail.com>
2026-01-13 08:19:14 +0200

    initial commit

---
+++ src/main/java/org/example/practic_hudea_lucian_722/PracticHudeaLucian722Application.java
@@ -0,0 +1,13 @@
+package org.example.practic_hudea_lucian_722;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticHudeaLucian722Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticHudeaLucian722Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/practic_hudea_lucian_722/PracticHudeaLucian722ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practic_hudea_lucian_722;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticHudeaLucian722ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
